0b4ae3f2349b4f91c9420d28e1f00e6f1186d730
Developer iX7 <78102400+DEVIX7@users.noreply.github.com>
2024-01-30 20:10:55 +0200

    Update android_roblox_auto_vip_server_join.py

--- android_roblox_auto_vip_server_join.py
+++ android_roblox_auto_vip_server_join.py
@@ -9,13 +9,25 @@ async def handle_client(websocket, path):
 
             if message == 'connect-to-vip-server':
                 print("Opening link...")
-                # Отправляем запрос на Google.com
                 subprocess.run(['termux-open-url', 'https://www.roblox.com/games/3260590327?privateServerLinkCode=45922069851868244203416146497318'])
 
     except websockets.exceptions.ConnectionClosedError:
         print(f"Client disconnected: {websocket.remote_address}")
 
 if __name__ == "__main__":
+    print('''
+   ___  _____   _______  ______
+  / _ \/ __/ | / /  _/ |/_/_  /
+ / // / _/ | |/ // /_>  <  / / 
+/____/___/ |___/___/_/|_| /_/ 
+     github.com/DEVIX7
+    ''')
+    try:
+        import websockets
+    except ImportError:
+        print("Module 'websockets' not found. Auto-installing...")
+        subprocess.run(['pip', 'install', 'websockets'])
+
     server = websockets.serve(handle_client, "localhost", 8126)
 
     print("WebSocket server started. Listening on ws://localhost:8126")
